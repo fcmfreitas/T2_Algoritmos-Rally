3137ecfb269210f8902149776189956ada9c8276
Luana Rauber Sostisso <luana.sostisso@hp.com>
2025-06-20 23:26:35 -0300

    adicionando segundo exemplo

--- App.java
+++ App.java
@@ -1,10 +1,26 @@
 public class App {
     public static void main(String[] args) {
-        Carro carro = new Carro(100); 
-        Rally rally = new Rally(carro, 500);
-
-        System.out.println(rally.printRally());
-
-        rally.SimularRally(carro);
+        
+        System.out.println("\n--- Rally 1: Rally 500km, carro com autonomia de 100km/dia ---");
+        Carro carro1 = new Carro(100);
+        long startTime1 = System.currentTimeMillis();
+        Rally rally1 = new Rally(carro1, 500);
+        System.out.println(rally1.printRally());
+        rally1.SimularRally(carro1);
+        long endTime1 = System.currentTimeMillis();
+        long totalTime1 = endTime1 - startTime1;
+        System.out.println("Distância final percorrida: " + carro1.getDistancia_percorrida() + " km");
+        System.out.println("Tempo de execução: " + totalTime1 + " ms");
+        
+        System.out.println("\n--- Rally 800km, carro com autonomia de 50km/dia ---");
+        Carro carro2 = new Carro(50);
+        long startTime3 = System.currentTimeMillis();
+        Rally rally2 = new Rally(carro2, 800);
+        System.out.println(rally2.printRally());
+        rally2.SimularRally(carro2);
+        long endTime3 = System.currentTimeMillis();
+        long totalTime3 = endTime3 - startTime3;
+        System.out.println("Distância final percorrida: " + carro2.getDistancia_percorrida() + " km");
+        System.out.println("Tempo de execução: " + totalTime3 + " ms");
     }
 }
